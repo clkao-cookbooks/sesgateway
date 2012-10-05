f81231a3cadb630a3f5cb7aeb0dab98fe41bb35b
Kyle Burckhard <talaris@gmail.com>
2012-10-05 13:39:58 -0700

    Add stunnel dependendency to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,7 @@ maintainer_email "clkao@clkao.org"
 license          "All rights reserved"
 description      "Configures AWS SES smtp gateway"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+
+# available @ http://community.opscode.com/cookbooks/stunnel
+depends "stunnel"
