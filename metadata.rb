fa107690e01ab2e43a91b6a5406aa03ec790e788
Kyle Burckhard <talaris@gmail.com>
2012-10-05 13:28:56 -0700

    Update metadata point to README.md

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Chia-liang Kao"
 maintainer_email "clkao@clkao.org"
 license          "All rights reserved"
 description      "Configures AWS SES smtp gateway"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
